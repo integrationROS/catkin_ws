362ec7a437fc412971fe4c3dfddd4bcaa9256d18
Loan <loan.bernat@univ-tlse3.fr>
2023-09-12 14:25:22 +0200

    recup old setup

--- build/hc10_moveit_config/catkin_generated/setup_cached.sh
+++ build/hc10_moveit_config/catkin_generated/setup_cached.sh
@@ -8,8 +8,6 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/etudiant/catkin_ws/devel/.private/hc10_moveit_config:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/etudiant/catkin_ws/devel/.private/hc10_moveit_config/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/etudiant/catkin_ws/devel/.private/hc10_moveit_config/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/etudiant/catkin_ws/build/hc10_moveit_config'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/etudiant/catkin_ws/devel/.private/hc10_moveit_config/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/etudiant/catkin_ws/src/hc10_moveit_config:$ROS_PACKAGE_PATH"
