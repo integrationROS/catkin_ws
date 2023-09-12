645b91deed66bd06d8b74a3110ffb83247863a74
Quentin <quentin.phan@univ-tlse3.fr>
2023-09-12 17:22:53 +0200

    init push

--- build/motoman_msgs/catkin_generated/setup_cached.sh
+++ build/motoman_msgs/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/etudiant/catkin_ws/devel/.private/motoman_msgs:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/etudiant/catkin_ws/devel/.private/motoman_msgs/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/etudiant/catkin_ws/devel/.private/motoman_msgs/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/etudiant/catkin_ws/build/motoman_msgs'
+export PYTHONPATH="/home/etudiant/catkin_ws/devel/.private/motoman_msgs/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/etudiant/catkin_ws/devel/.private/motoman_msgs/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/etudiant/catkin_ws/src/motoman/motoman_msgs:$ROS_PACKAGE_PATH"
