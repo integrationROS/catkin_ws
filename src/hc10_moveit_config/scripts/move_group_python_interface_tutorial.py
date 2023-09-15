78395ce8d5abef737a4203878ef324890a26046a
simonrulof <simonber2001@gmail.com>
2023-09-15 17:13:08 +0200

    almost finished advanced trajectory

---
+++ src/hc10_moveit_config/scripts/move_group_python_interface_tutorial.py
@@ -0,0 +1,422 @@
+#!/usr/bin/env python3
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2013, SRI International
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of SRI International nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Acorn Pooley, Mike Lautman
+
+## BEGIN_SUB_TUTORIAL imports
+##
+## To use the Python MoveIt interfaces, we will import the `moveit_commander`_ namespace.
+## This namespace provides us with a `MoveGroupCommander`_ class, a `PlanningSceneInterface`_ class,
+## and a `RobotCommander`_ class. More on these below. We also import `rospy`_ and some messages that we will use:
+##
+
+# Python 2/3 compatibility imports
+from __future__ import print_function
+from audioop import lin2alaw
+from locale import normalize
+from tkinter import N
+from turtle import pos, position
+from six.moves import input
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+import numpy as np
+from math import pi, tau, dist, fabs, cos, sin
+
+tau = 2.0 * pi
+
+
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+
+xRobot = 0.55
+yRobot = 0.4
+zRobot = 0.16
+
+xObjet = 1.05
+yObjet = 0.9
+zObjet = 0.25
+
+## END_SUB_TUTORIAL
+
+def all_close(goal, actual, tolerance):
+    """
+    Convenience method for testing if the values in two lists are within a tolerance of each other.
+    For Pose and PoseStamped inputs, the angle between the two quaternions is compared (the angle
+    between the identical orientations q and -q is calculated correctly).
+    @param: goal       A list of floats, a Pose or a PoseStamped
+    @param: actual     A list of floats, a Pose or a PoseStamped
+    @param: tolerance  A float
+    @returns: bool
+    """
+    if type(goal) is list:
+        for index in range(len(goal)):
+            if abs(actual[index] - goal[index]) > tolerance:
+                return False
+
+    elif type(goal) is geometry_msgs.msg.PoseStamped:
+        return all_close(goal.pose, actual.pose, tolerance)
+
+    elif type(goal) is geometry_msgs.msg.Pose:
+        x0, y0, z0, qx0, qy0, qz0, qw0 = pose_to_list(actual)
+        x1, y1, z1, qx1, qy1, qz1, qw1 = pose_to_list(goal)
+        # Euclidean distance
+        d = dist((x1, y1, z1), (x0, y0, z0))
+        # phi = angle between orientations
+        cos_phi_half = fabs(qx0 * qx1 + qy0 * qy1 + qz0 * qz1 + qw0 * qw1)
+        return d <= tolerance and cos_phi_half >= cos(tolerance / 2.0)
+
+    return True
+
+
+
+class MoveGroupPythonInterfaceTutorial(object):
+    """MoveGroupPythonInterfaceTutorial"""
+
+    def __init__(self):
+        super(MoveGroupPythonInterfaceTutorial, self).__init__()
+
+        ## BEGIN_SUB_TUTORIAL setup
+        ##
+        ## First initialize `moveit_commander`_ and a `rospy`_ node:
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node("move_group_python_interface_tutorial", anonymous=True)
+
+        ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
+        ## kinematic model and the robot's current joint states
+        robot = moveit_commander.RobotCommander()
+
+        ## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
+        ## for getting, setting, and updating the robot's internal understanding of the
+        ## surrounding world:
+        scene = moveit_commander.PlanningSceneInterface()
+
+        ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+        ## to a planning group (group of joints).  In this tutorial the group is the primary
+        ## arm joints in the Panda robot, so we set the group's name to "panda_arm".
+        ## If you are using a different robot, change this value to the name of your robot
+        ## arm planning group.
+        ## This interface can be used to plan and execute motions:
+        group_name = "hc10_arm"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
+
+        ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
+        ## trajectories in Rviz:
+        display_trajectory_publisher = rospy.Publisher(
+            "/move_group/display_planned_path",
+            moveit_msgs.msg.DisplayTrajectory,
+            queue_size=20,
+        )
+
+        ## END_SUB_TUTORIAL
+
+        ## BEGIN_SUB_TUTORIAL basic_info
+        ##
+        ## Getting Basic Information
+        ## ^^^^^^^^^^^^^^^^^^^^^^^^^
+        # We can get the name of the reference frame for this robot:
+        planning_frame = move_group.get_planning_frame()
+        print("============ Planning frame: %s" % planning_frame)
+
+        # We can also print the name of the end-effector link for this group:
+        eef_link = move_group.get_end_effector_link()
+        print("============ End effector link: %s" % eef_link)
+
+        # We can get a list of all the groups in the robot:
+        group_names = robot.get_group_names()
+        print("============ Available Planning Groups:", robot.get_group_names())
+
+        # Sometimes for debugging it is useful to print the entire state of the
+        # robot:
+        print("============ Printing robot state")
+        print(robot.get_current_state())
+        print("")
+        ## END_SUB_TUTORIAL
+
+        # Misc variables
+        self.box_name = ""
+        self.robot = robot
+        self.scene = scene
+        self.move_group = move_group
+        self.display_trajectory_publisher = display_trajectory_publisher
+        self.planning_frame = planning_frame
+        self.eef_link = eef_link
+        self.group_names = group_names
+
+    def go_to_joint_state(self, joint_0, joint_1, joint_2, joint_3, joint_4, joint_5):
+        # Copy class variables to local variables to make the web tutorials more clear.
+        # In practice, you should use the class variables directly unless you have a good
+        # reason not to.
+        move_group = self.move_group
+
+        ## BEGIN_SUB_TUTORIAL plan_to_joint_state
+        ##
+        ## Planning to a Joint Goal
+        ## ^^^^^^^^^^^^^^^^^^^^^^^^
+        ## The Panda's zero configuration is at a `singularity <https://www.quora.com/Robotics-What-is-meant-by-kinematic-singularity>`_, so the first
+        ## thing we want to do is move it to a slightly better configuration.
+        ## We use the constant `tau = 2*pi <https://en.wikipedia.org/wiki/Turn_(angle)#Tau_proposals>`_ for convenience:
+        # We get the joint values from the group and change some of the values:
+        joint_goal = move_group.get_current_joint_values()
+        joint_goal[0] = joint_0
+        joint_goal[1] = joint_1
+        joint_goal[2] = joint_2
+        joint_goal[3] = joint_3
+        joint_goal[4] = joint_4
+        joint_goal[5] = joint_5
+        print(joint_goal)
+        # The go command can be called with joint values, poses, or without any
+        # parameters if you have already set the pose or joint target for the group
+        move_group.go(joint_goal, wait=True)
+
+        # Calling ``stop()`` ensures that there is no residual movement
+        move_group.stop()
+
+        ## END_SUB_TUTORIAL
+
+        # For testing:
+        current_joints = move_group.get_current_joint_values()
+        print(move_group.get_current_pose())
+
+        return all_close(joint_goal, current_joints, 0.01)
+
+    def go_to_pose_goal(self, orientation_w = 0.11120193534490344, orientation_x = -0.7074742063308836, orientation_y= -0.679324449298975, orientation_z = -0.16010205975151776, position_x = 0.11026193016919225, position_y = 0.3365024822974074, position_z = 1.432950792334329, xRobot = 0, yRobot = 0, zRobot = 0):
+        # Copy class variables to local variables to make the web tutorials more clear.
+        # In practice, you should use the class variables directly unless you have a good
+        # reason not to.
+        move_group = self.move_group
+
+        ## BEGIN_SUB_TUTORIAL plan_to_pose
+        ##
+        ## Planning to a Pose Goal
+        ## ^^^^^^^^^^^^^^^^^^^^^^^
+        ## We can plan a motion for this group to a desired pose for the
+        ## end-effector:
+        """
+        x: 0.11026193016919225
+        y: 0.3365024822974074
+        z: 1.432950792334329
+        orientation: 
+        x: -0.7074742063308836
+        y: -0.679324449298975
+        z: -0.16010205975151776
+        w: 0.11120193534490344
+        """
+
+        pose_goal = geometry_msgs.msg.Pose()
+        pose_goal.orientation.w = orientation_w
+        pose_goal.orientation.x = orientation_x
+        pose_goal.orientation.y = orientation_y
+        pose_goal.orientation.z = orientation_z
+        pose_goal.position.x = position_x 
+        pose_goal.position.y = position_y 
+        pose_goal.position.z = position_z 
+        print(move_group.get_current_pose())
+        print("----------------------")
+        print(pose_goal)
+        print("++++++++++++++++++++++")
+
+
+        move_group.set_pose_target(pose_goal)
+
+        ## Now, we call the planner to compute the plan and execute it.
+        # `go()` returns a boolean indicating whether the planning and execution was successful.
+        success = move_group.go(wait=True)
+        # Calling `stop()` ensures that there is no residual movement
+        move_group.stop()
+        # It is always good to clear your targets after planning with poses.
+        # Note: there is no equivalent function for clear_joint_value_targets().
+        move_group.clear_pose_targets()
+
+        ## END_SUB_TUTORIAL
+
+        # For testing:
+        # Note that since this section of code will not be included in the tutorials
+        # we use the class variable rather than the copied state variable
+        current_pose = self.move_group.get_current_pose().pose
+        return all_close(pose_goal, current_pose, 0.01)
+
+    def display_trajectory(self, plan):
+        # Copy class variables to local variables to make the web tutorials more clear.
+        # In practice, you should use the class variables directly unless you have a good
+        # reason not to.
+        robot = self.robot
+        display_trajectory_publisher = self.display_trajectory_publisher
+
+        ## BEGIN_SUB_TUTORIAL display_trajectory
+        ##
+        ## Displaying a Trajectory
+        ## ^^^^^^^^^^^^^^^^^^^^^^^
+        ## You can ask RViz to visualize a plan (aka trajectory) for you. But the
+        ## group.plan() method does this automatically so this is not that useful
+        ## here (it just displays the same trajectory again):
+        ##
+        ## A `DisplayTrajectory`_ msg has two primary fields, trajectory_start and trajectory.
+        ## We populate the trajectory_start with our current robot state to copy over
+        ## any AttachedCollisionObjects and add our plan to the trajectory.
+        display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+        display_trajectory.trajectory_start = robot.get_current_state()
+        display_trajectory.trajectory.append(plan)
+        # Publish
+        display_trajectory_publisher.publish(display_trajectory)
+
+        ## END_SUB_TUTORIAL
+
+    def circle(self, a, b, c, nb_pts = 8, r = 0.2, xRobot = 0, yRobot = 0, zRobot = 0):
+        move_group = self.move_group        
+        for i in range(0,nb_pts):
+            t = (tau *i)/nb_pts
+            x = r*cos(t)+a
+            y = r*sin(t)+b
+            z = c+r
+            vec_x = (a-x)
+            vec_y = (b-y)
+            vec_z = (c-z)
+            vec=(vec_x, vec_y, vec_z)
+            normalized_vec=vec/np.linalg.norm(vec)
+            print(x)
+            print(y)
+            print(z)
+
+            print(normalized_vec)
+            """move_group.set_position_target((x,y,z))
+            ## Now, we call the planner to compute the plan and execute it.
+            # `go()` returns a boolean indicating whether the planning and execution was successful.
+            success = move_group.go(wait=True)
+            # Calling `stop()` ensures that there is no residual movement
+            move_group.stop()"""
+            self.go_to_pose_goal(position_x=x, position_y=y, position_z=z, orientation_x=normalized_vec[0], orientation_y=normalized_vec[1], orientation_z=normalized_vec[2], orientation_w=0, xRobot= xRobot, yRobot= yRobot, zRobot= zRobot)
+
+    def test_path(self, x,y,z):
+        move_group = self.move_group
+        move_group.set_position_target((x,y,z))
+        ## Now, we call the planner to compute the plan and execute it.
+        # `go()` returns a boolean indicating whether the planning and execution was successful.
+        success = move_group.go(wait=True)
+        # Calling `stop()` ensures that there is no residual movement
+        move_group.stop()
+        # It is always good to clear your targets after planning with poses.
+        # Note: there is no equivalent function for clear_joint_value_targets().
+        move_group.clear_pose_targets()
+
+def main():
+    try:
+        print("")
+        print("----------------------------------------------------------")
+        print("Welcome to the MoveIt MoveGroup Python Interface Tutorial")
+        print("----------------------------------------------------------")
+        print("Press Ctrl-D to exit at any time")
+        print("")
+        input(
+            "============ Press `Enter` to begin the tutorial by setting up the moveit_commander ..."
+        )
+        tutorial = MoveGroupPythonInterfaceTutorial()
+
+        
+
+        
+        input(
+            "============ Press `Enter` to execute a movement using a joint state goal ..."
+        )
+        # [1.71829990882053, 0.2981078404806903, 0.6413125751706046, 0.00010567234931535552, -0.6768992005721213, -9.200472028680906e-08]
+        joint_0 = 1.7183
+        joint_1 = 0.2981
+        joint_2 = 0.6413
+        joint_3 = 0
+        joint_4 = -0.6768
+        joint_5 = 0 
+        print(tutorial.move_group.get_current_joint_values())
+        tutorial.go_to_joint_state(joint_0, joint_1,joint_2, joint_3, joint_4, joint_5)
+
+        input(
+            "============ Press `Enter` to execute a movement using a circle ..."
+        )
+        #tutorial.go_to_pose_goal(position_x=0.3347, position_y=0.7466, position_z=0.9, orientation_x=-0.12688213932436357, orientation_y=0.1094082298369706, orientation_z=0.7466387417213558, orientation_w=0.6437867281368606)
+        tutorial.circle(a=xObjet, b=yObjet,c=zObjet, xRobot= xRobot, yRobot= yRobot, zRobot= zRobot)
+        # tutorial.test_path(0.3,0.3,1.0)
+       
+        input("============ Press `Enter` to execute a movement using a pose goal ...")
+        tutorial.go_to_pose_goal()
+
+        print("============ Completed!")
+    except rospy.ROSInterruptException:
+        return
+    except KeyboardInterrupt:
+        return
+
+
+if __name__ == "__main__":
+    main()
+
+## BEGIN_TUTORIAL
+## .. _moveit_commander:
+##    http://docs.ros.org/noetic/api/moveit_commander/html/namespacemoveit__commander.html
+##
+## .. _MoveGroupCommander:
+##    http://docs.ros.org/noetic/api/moveit_commander/html/classmoveit__commander_1_1move__group_1_1MoveGroupCommander.html
+##
+## .. _RobotCommander:
+##    http://docs.ros.org/noetic/api/moveit_commander/html/classmoveit__commander_1_1robot_1_1RobotCommander.html
+##
+## .. _PlanningSceneInterface:
+##    http://docs.ros.org/noetic/api/moveit_commander/html/classmoveit__commander_1_1planning__scene__interface_1_1PlanningSceneInterface.html
+##
+## .. _DisplayTrajectory:
+##    http://docs.ros.org/noetic/api/moveit_msgs/html/msg/DisplayTrajectory.html
+##
+## .. _RobotTrajectory:
+##    http://docs.ros.org/noetic/api/moveit_msgs/html/msg/RobotTrajectory.html
+##
+## .. _rospy:
+##    http://docs.ros.org/noetic/api/rospy/html/
+## CALL_SUB_TUTORIAL imports
+## CALL_SUB_TUTORIAL setup
+## CALL_SUB_TUTORIAL basic_info
+## CALL_SUB_TUTORIAL plan_to_joint_state
+## CALL_SUB_TUTORIAL plan_to_pose
+## CALL_SUB_TUTORIAL plan_cartesian_path
+## CALL_SUB_TUTORIAL display_trajectory
+## CALL_SUB_TUTORIAL execute_plan
+## CALL_SUB_TUTORIAL add_box
+## CALL_SUB_TUTORIAL wait_for_scene_update
+## CALL_SUB_TUTORIAL attach_object
+## CALL_SUB_TUTORIAL detach_object
+## CALL_SUB_TUTORIAL remove_object
+## END_TUTORIAL
